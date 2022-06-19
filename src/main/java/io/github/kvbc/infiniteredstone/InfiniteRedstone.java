fb20f44b2d5dc5eaf67e8a9f3277115ae93e3039
Matt <64214044+kvbc@users.noreply.github.com>
2022-06-19 11:32:35 +0200

    Update InfiniteRedstone.java

--- src/main/java/io/github/kvbc/infiniteredstone/InfiniteRedstone.java
+++ src/main/java/io/github/kvbc/infiniteredstone/InfiniteRedstone.java
@@ -4,10 +4,9 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,10 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class InfiniteRedstone extends JavaPlugin implements Listener {
-    private List<Location> locations = new ArrayList<Location>();
+    private final long REDSTONE_REFRESH_DELAY = 0; // in ticks
+    private final List<Location> locations = new ArrayList<Location>();
 
-    private void clear_locations () {
-        HandlerList.unregisterAll((Plugin)this);
+    private void refresh_redstone () {
+        BlockRedstoneEvent.getHandlerList().unregister((Listener)this);
 
         List<Location> locationsCopy = new ArrayList<Location>(locations);
         locations.clear();
@@ -41,9 +41,9 @@ public final class InfiniteRedstone extends JavaPlugin implements Listener {
         new BukkitRunnable() {
             @Override
             public void run() {
-                clear_locations();
+                refresh_redstone();
             }
-        }.runTaskTimer(this, 0, 0);
+        }.runTaskTimer(this, 0, REDSTONE_REFRESH_DELAY);
     }
 
     @EventHandler
@@ -52,8 +52,17 @@ public final class InfiniteRedstone extends JavaPlugin implements Listener {
             if ((event.getNewCurrent() > 0) && (event.getNewCurrent() < 15)) {
                 event.setNewCurrent(15);
                 if(!locations.contains(event.getBlock().getLocation()))
-                    locations.add(event.getBlock().getLocation());
+                    locations.add(0, event.getBlock().getLocation());
             }
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak (BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (block.getType() == Material.REDSTONE_WIRE) {
+            if (locations.contains(block.getLocation()))
+                locations.remove(block.getLocation());
         }
     }
 }
